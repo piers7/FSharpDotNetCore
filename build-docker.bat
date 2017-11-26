86ec85d423630a69a23aec26d5bc384b85ead4b3
Piers Williams <piers.williams@smsmt.com>
2017-11-26 16:02:18 +0800

    Create seperate files for local and docker builds

---
+++ build-docker.bat
@@ -0,0 +1,3 @@
+@echo off
+docker build -t fsharpdotnetcore .\FSharpDotNetCore %*
+docker run --rm -p 8080:80 fsharpdotnetcore
--- build.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-
-
-docker build -t fsharpdotnetcore .\FSharpDotNetCore --no-cache
-docker run --rm -p 8080:80 fsharpdotnetcore
